05e0d27d246ec3afc3b59e2c86756b326ec43548
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-03-26 13:09:49 +0100

    kilka małych zmian w sposobie wyświetlania ksiąg

--- test.js
+++ test.js
@@ -5,11 +5,13 @@
 const lektura = require('./lektura')
 const biblia = require('./biblia')
 
-const today = new Date()
-const dd = String(today.getDate()).padStart(2, "0")
-const mm = String(today.getMonth() + 1).padStart(2, "0")
+const now = new Date()
+const today = [
+    String(now.getMonth() + 1).padStart(2, "0"),
+    String(now.getDate()).padStart(2, "0"),
+].join("-")
 
-const lekturaNaDzis = lektura[`${mm}-${dd}`]
+const lekturaNaDzis = lektura[today]
 
 console.log("-------------------------------")
 console.log("Dzisiaj do przeczytania mam:")
@@ -18,8 +20,10 @@ console.log("-------------------------------")
 
 lekturaNaDzis?.forEach(ksiega => {
     console.log(ksiega)
+    let display = ""
     biblia[ksiega]?.forEach((wers, i) => {
-        console.log((i+1), wers)
+        display += `|${i+1}| ${wers} `
     })
+    console.log(display)
     console.log("-------------------------------")
 })
