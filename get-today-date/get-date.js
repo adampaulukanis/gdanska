ec7e8cf9335293e744afc3eb5e5eff873f4e0e1c
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-08-03 01:08:05 +0100

    Mała poprawka

--- get-today-date/get-date.js
+++ get-today-date/get-date.js
@@ -5,12 +5,10 @@ function getDate(today) {
     if (today) {
         now = new Date(today);
     }
-    today = [
+    return today = [
         String(now.getMonth() + 1).padStart(2, "0"),
         String(now.getDate()).padStart(2, "0"),
     ].join("-");
-
-    return today;
 };
 
 module.exports = getDate;
