d346cbd80c16c12edf592bd743cefc0d199040d3
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-09-03 15:43:21 +0100

    Pora pozbyć się tego

--- parser/parser-test.js
+++ parser/parser-test.js
@@ -5,15 +5,45 @@ const assert = require("node:assert").strict;
 const Parser = require("./parser");
 
 describe("Mój parser", () => {
-    it("1Moj1 OK",  () => {
-        assert.deepEqual(Parser("1Moj1"), [ "1Moj1" ]);
+    let db = {};
+
+    /*
+    it("test1", () => {
+        let p = new Parser("haha-kaszanka");
+        p.init("siemanko");
+    */
+    /*
+        assert.deepEqual(
+            Parser('test1'),
+            null);
+    });
+    */
+    it("1Moj1",  () => {
+        db["1Moj1"] = [ "1Moj1" ];
+        db["xfilez"] = [ "Kaszanka PL" ]; // TODO: Potrzebne mi to? `M
+        assert.deepEqual(Parser("1Moj1"), db);
+        delete db["1Moj1"];
+        // nie trzeba usuwac xfilez
     })
 
-    it("ŁUK17 OK, (unicode)", () => {
-        assert.deepEqual(Parser("ŁUK17"), [ "ŁUK17" ]);
+    it("ŁUK17, (unicode)", () => {
+        // db xfilez implicite zadeklarowane
+        db["ŁUK17"] = [ "ąćęłńóśżź" ]; // TODO: How to make sure this is in UNICODE?
+        assert.deepEqual(Parser("ŁUK17"), db);
+        assert.deepEqual(Parser("ŁUK17")["ŁUK17"][0][4] === "ń", true);
+        delete db["ŁUK17"];
     })
 
-    it("Joz12-15 OK", () => {
+    /*
+     * Nie jestem jeszcze gotowy na zmienna *wiecej*
+    it("Joz12-15", () => {
         assert.deepEqual(Parser("Joz12-15"), [ "Joz12", "Joz13", "Joz14", "Joz15" ]);
     })
+
+    describe("For debug", () => {
+        it("Adam1", () => { assert.deepEqual(Parser("Adam1"), [ "Adam1" ]); })
+        it("Adam12", () => { assert.deepEqual(Parser("Adam12"), [ "Adam12" ]); })
+        it("1Adam12", () => { assert.deepEqual(Parser("1Adam12"), [ "1Adam12" ]); })
+    })
+    */
 })
--- parser/parser.js
+++ parser/parser.js
@@ -8,13 +8,32 @@
  *      ...
  *      wersN
  * ]
-*/
-function Parser(string){
-    let retArr = [];
-    const re = /^([\p{L}\p{N}]+)(-\d+)*/gu;
+ */
+
+const db = {
+    "test1": [
+        "Line one.",
+        "Line two"
+    ],
+    "1Moj1": [ "1Moj1" ],
+    "ŁUK17": [ "ąćęłńóśżź" ],
+};
+
+function Parser(string) {
+    //console.log(`Hit again Parser(string=${string})`);
+    let retArr = {};
+    const re = /^([\p{L}\p{N}]+)(-\d+)*/gu; // TODO: What is a correct one?
     let [ _, rozdzial, wiecej ] = re.exec(string);
 
-    retArr.push(rozdzial);
+    //console.log({ _, rozdzial, wiecej });
+
+    retArr[rozdzial] = db[rozdzial];
+    retArr["xfilez"] = [ "Kaszanka PL" ]; // TODO: Potrzebne mi to? `N
+
+    return retArr;
+
+    //////////////////////////////////////////////////////////////////////
+    // TODO: do zrobienia ;-)
 
     if (wiecej) {
         const nazwa = rozdzial.replace(/\d+$/, "");
