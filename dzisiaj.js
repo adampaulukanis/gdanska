983fab5d99ce69dd8620af724bade96bf02891ce
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-10-27 23:30:30 +0100

    Pokazuj krótkie info na temat księgi (nazwa księgi)

--- dzisiaj.js
+++ dzisiaj.js
@@ -6,11 +6,18 @@ const biblia = require("./biblia");
 const MM_DD = require('./get-MM-DD/get-date.js');
 let lekturaNaDzis = require("./kalendarium")[MM_DD()];
 const Parser = require("parser");
+const Skroty = require("./skroty.json");
+
+function getSkrot(tytul) {
+    return tytul.split(/([0-9]*[a-zA-Z]+)/u)[1];
+}
 
 for (const rozdzial of lekturaNaDzis) {
     for (const tytul of Parser(rozdzial)) {
+        let fajnaNazwa = getSkrot(tytul)
+        console.log(fajnaNazwa);
         console.log("--------------------------");
-        console.log(`${tytul} [${rozdzial}]`);
+        console.log(`${tytul} [${rozdzial}] ${Skroty[fajnaNazwa]}`);
         console.log("--------------------------");
 
         const wersy = biblia[tytul] || [ "error", tytul, "niedostępna w biblia.json" ];
